864d54dda0f7b0c7dab6d6d759185543f9b3ae13
sfujimotosfujimoto <unicoshun@gmail.com>
2023-03-23 16:14:53 +0900

    add path consoles

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -19,6 +19,15 @@ export async function markdownToHtml(
 ) {
   const { content, data } = matter(markdown)
   const themeFile = await glob("./themes/rose-pine-moon.json")
+  console.log("🚀 lib/markdown.ts ~ 	🌈 themeFile ✨ ", themeFile)
+
+  const path1 = path.join(__dirname)
+  console.log("🚀 lib/markdown.ts ~ 	🌈 path1 __dirname ✨ ", path1)
+  const path2 = process.cwd()
+  console.log("🚀 lib/markdown.ts ~ 	🌈 path2 cwd✨ ", path2)
+
+  const path3 = await glob("./*")
+  console.log('🚀 lib/markdown.ts ~ 	🌈 path3 glob("./*")✨ ', path3)
 
   if (!themeFile[0]) {
     throw new Error("Couldn't get theme file")
